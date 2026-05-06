6ca28d41992bd0c2c0fda983d92bd2fa3f9428ff
micaelamol <vanesamolina315@gmail.com>
2026-05-06 18:04:54 -0300

    Servidor funcionando y conexión DB

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+//import especialidadesRouter from "./src/routers/especialidadesRouter.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Rutas
+//app.use("/api/v1/especialidades", especialidadesRouter);
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.json({ estado: "OK", msg: "Servidor funcionando" });
+});
+
+// Inicializar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
