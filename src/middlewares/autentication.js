8b4228a4414d3b86688f546b749ac885410bc099
carlos anibal gabrovich <carlos.gabrovich@gmail.com>
2026-06-14 11:26:40 -0300

    corrijo el login para que sea compatible con swagger

--- src/middlewares/autentication.js
+++ src/middlewares/autentication.js
@@ -1,15 +1,31 @@
 import jwt from "jsonwebtoken";
+import { Usuarios } from "../db/usuarios.js";
+import { createClient } from "redis";
 
 
-export function autentication(req, res, next) {
+export async function autentication(req, res, next) {
   req.user = { id_usuario: "", usuario: "", rol: "" };
   try {
-    const token = req.cookies["acces-token"];
+    const token = req.headers['authorization']?.split(' ')[1] 
+    
     if (token) {
-      //console.log("Verificando el token JWT... ", token);
+      
       const datos = jwt.verify(token, process.env.SECRET_KEY);
-      console.log("Datos decodificados del token JWT: ", datos);
-      req.user = { id_usuario: datos.payload.id_usuario, usuario: datos.payload.usuario, rol: datos.payload.rol };
+      
+
+      const client = await createClient({url: 'redis://default:RO4vsO0cjg4jHlpFaNbKGaO0T5qUANev@tendency-island-grade-36016.db.redis.io:13527'});
+      await client.connect();
+      const estaAnulado = await client.exists(`revoked_${token}`) === 1;
+      
+      await client.quit();
+
+      if (estaAnulado) {
+        return res.status(401).send("Token inválido: el token ha sido anulado");
+      }
+      const usuario = async () => await Usuarios.obtenerUsuarioPorId(datos.id_usuario);
+      if (usuario) {
+        req.user = { id_usuario: datos.id_usuario, usuario: datos.usuario, rol: datos.rol };
+      }
 
     }
   } catch (error) {
