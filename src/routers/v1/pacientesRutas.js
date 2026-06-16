49bd194ec0f344b6c19edafba374b0ff0f4a54a3
[paola] <[pvpenalvarebaque@gmail.com]>
2026-06-15 21:42:57 -0300

    Actualizo rutas de usuarios y pacientes desde paola

--- src/routers/v1/pacientesRutas.js
+++ src/routers/v1/pacientesRutas.js
@@ -2,15 +2,59 @@ import { Router } from "express";
 import { check, param } from "express-validator";
 import { pacientesController } from "../../controllers/pacientesControlador.js";
 import { validarCampos } from "../../middlewares/validarCampos.js";
+import { autorizarUsuarios } from "../../middlewares/autorizarUsuarios.js";
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Pacientes
+ *   description: Endpoints para gestionar pacientes
+ */
+
+/**
+ * @swagger
+ * /api/v1/pacientes:
+ *   get:
+ *     summary: Obtener todos los pacientes
+ *     tags: [Pacientes]
+ *     responses:
+ *       200:
+ *         description: Lista de pacientes
+ */
+
+
 //GET
-router.get("/", pacientesController.obtenerPacientes);
+router.get(
+  "/", autorizarUsuarios([1,3]), 
+  pacientesController.obtenerPacientes);
+
+
+
+/**
+ * @swagger
+ * /api/v1/pacientes/{id_paciente}:
+ *   get:
+ *     summary: Obtener un paciente por ID
+ *     tags: [Pacientes]
+ *     parameters:
+ *       - in: path
+ *         name: id_paciente
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Paciente encontrado
+ *       404:
+ *         description: Paciente no encontrado
+ */
 
 //Get paciente por ID
 router.get(
- "/:id_paciente",
+ "/:id_paciente", autorizarUsuarios([1,3]),
  [
  param("id_paciente", "El ID debe ser un número entero").isInt(),
  validarCampos
@@ -18,6 +62,9 @@ router.get(
  pacientesController.obtenerPacientePorId
 
 );
+
+
+
 
 //POST  crear paciente
 router.post(
@@ -31,9 +78,44 @@ router.post(
  .withMessage("El id_obra_social debe ser un número entero."),
 
  validarCampos
-],
+], autorizarUsuarios([1,3]),
 pacientesController.crearPaciente
 );
+
+
+
+
+
+
+/**
+ * @swagger
+ * /api/v1/pacientes/{id_paciente}:
+ *   put:
+ *     summary: Modificar paciente
+ *     tags: [Pacientes]
+ *     parameters:
+ *       - in: path
+ *         name: id_paciente
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id_obra_social:
+ *                 type: integer
+ *                 example: 10
+ *     responses:
+ *       200:
+ *         description: Paciente actualizado correctamente
+ *       404:
+ *         description: Paciente no encontrado
+ */
+
 
 //PUT modificar paciente
 router.put(
@@ -46,13 +128,34 @@ router.put(
       .withMessage("El id_obra_social debe ser un número entero."),
 
     validarCampos
-  ],
+  ], autorizarUsuarios([1,3]),
   pacientesController.actualizarPaciente
 );
 
+
+
+/**
+ * @swagger
+ * /api/v1/pacientes/{id_paciente}:
+ *   delete:
+ *     summary: Eliminar un paciente
+ *     tags: [Pacientes]
+ *     parameters:
+ *       - in: path
+ *         name: id_paciente
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Paciente eliminado con éxito
+ *       404:
+ *         description: Paciente no encontrado
+ */
+
 //DELETE paciente
 router.delete(
- "/:id_paciente",
+ "/:id_paciente",autorizarUsuarios([1,3]),
  [
  param("id_paciente", "El ID debe ser un número entero").isInt(),
  validarCampos
